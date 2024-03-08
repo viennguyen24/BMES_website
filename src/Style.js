40e8d80a99b607e8e19c24235085e922aea674cb
unknown <viennguyen@usf.edu>
2024-03-08 14:45:07 -0500

    added boostrap

---
+++ src/Style.js
@@ -0,0 +1,6 @@
+export const COLOR = {
+  "light-blue": "#A9CDFDff",
+  "light-white": "#F2F2F2",
+  "heavy-blue": "#1F56AFff",
+  "heavy-dark": "#2B2928ff",
+};
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1>Welcome to USF Biomedical Engineering</h1>
+      <p>A rapidly growing and ever-evolving field of study</p>
+      <Button>Learn more</Button>
+    </div>
+  );
+}
