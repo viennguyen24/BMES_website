0f399e4de32ae2739448352b28d2604fee647b72
unknown <viennguyen@usf.edu>
2024-03-09 12:40:15 -0500

    added shadow to feature card

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -3,7 +3,11 @@ import Card from "react-bootstrap/Card";
 export default function Feature(props) {
   const { title, text, img } = props;
   return (
-    <Card>
+    <Card
+      style={{
+        boxShadow: "2.5px 2px 6.5px rgba(0, 0, 0, 0.25)",
+      }}
+    >
       <Card.Img variant="top" src={img} fluid rounded />
       <Card.Body>
         <Card.Title className="fw-bolder">{title}</Card.Title>
