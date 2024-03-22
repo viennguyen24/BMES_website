31283fd93b6556ee12c40c3442e068ceec43a883
unknown <viennguyen@usf.edu>
2024-03-22 00:58:30 -0400

    full pages and navigation added

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,11 @@
-import {Nav, Navbar, NavDropdown, Container, Image} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Container, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../assets/BMES_logo.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 export default function Header() {
   return (
     <header>
-      <Navbar expand="sm" role="navigation">
+      <Navbar expand="sm" role="navigation" fixed>
         <Container fluid="lg">
           <Navbar.Brand href="#home">
             <Image src={logo} roundedCircle style={{ maxWidth: "6.5rem" }} />
@@ -12,18 +13,38 @@ export default function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" label="navigation" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">About us</Nav.Link>
-              <Nav.Link href="#project">Our projects</Nav.Link>
-              <Nav.Link href="#officers">Officers</Nav.Link>
-              <Nav.Link href="#resource">Resources</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                About us
+              </Nav.Link>
+              <Nav.Link as={Link} to="/research">
+                R&D
+              </Nav.Link>
+              <Nav.Link as={Link} to="/people">
+                Our People
+              </Nav.Link>
+              <NavDropdown title="Resources">
+                <NavDropdown.Item href="https://www.bmes.org/">
+                  National BMES Site
+                </NavDropdown.Item>
+                <NavDropdown.Item href="">Academic Resources</NavDropdown.Item>
+                <NavDropdown.Item href="">Sponsors</NavDropdown.Item>
+              </NavDropdown>
               <NavDropdown title="Contacts">
-                <NavDropdown.Item>Instagram</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.instagram.com/bmesusf/">
+                  Instagram
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item>LinkedIn</NavDropdown.Item>
+                <NavDropdown.Item href="https://t.me/joinchat/LVJG4pcZr89hNzdh">
+                  Telegram
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item>Discord</NavDropdown.Item>
+                <NavDropdown.Item href="https://discord.com/invite/dYsxmJTwjJ">
+                  Discord
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item>BullsConnect</NavDropdown.Item>
+                <NavDropdown.Item href="https://bullsconnect.usf.edu/BMES/club_signup">
+                  BullsConnect
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
---
+++ src/pages/Research.js
@@ -0,0 +1,46 @@
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+export default function Research() {
+  return (
+    <>
+      <Header />
+      <main>
+        <article>
+          <h1>2022-2023 Project</h1>
+          <p>
+            BMES R&D provides an opportunity for students to learn various
+            technical ans soft skills in the field of biomedical engineering and
+            it's related disciplines. These skills are often used in many of the
+            upper level classes, such as MATLAB programming, Arduino coding and
+            circuit design, signal aquisition and noise reduction, teamwork and
+            communication, and general anatomy and physiology, among others. For
+            Engineering Expo, each project is carefully catered to the audience
+            of young children and teens, with the hope of teaching and inspiring
+            them to be more involved and interested in STEM. For more
+            information, contact the Research and Development Projects Chair{" "}
+          </p>
+        </article>
+        <article>
+          <h1>2022-2023 Competitive</h1>
+          <p>
+            For the 2022-2023 year, the Competitive Research and Development VEX
+            U team will work together to complete the next VEX challenge. This
+            challenge, or "Spin Up", consists of a primary objective to shoot
+            foam disks into elevated goals, or high goals, on corners to the
+            playing field. The teams will have two robots each, and can earn
+            points by not only shooting the disks, but also by spinning rollers,
+            and building the robotrs to expand on the field. We will be
+            utilizing metal materials (aluminum channels and plates, screws,
+            etc), motors, the VEXCode V5 software and other advanced materials
+            to build our robots. Our goal is to make it as fas as we can in the
+            VEC Championships. This is a safe learning environment and everyone
+            is expected to be helpful and accepting towards each other. For more
+            information, contact the Research and Development Competitive Chair
+            Sachita.
+          </p>
+        </article>
+      </main>
+      <Footer />
+    </>
+  );
+}
